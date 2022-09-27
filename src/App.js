27de591b18592522bb0974d81b678b11d0e89211
Karan Kapoor <kk8658141@gmail.com>
2022-09-27 13:37:09 +0530

    try

--- src/App.js
+++ src/App.js
@@ -4,17 +4,40 @@ function App() {
 
   const [data , setData] = useState([{}]);
   useEffect(()=>{
-    async function infohandler()
-    {
-      await axios.get("https://studentdb.pythonanywhere.com/students/",{}).then((res)=>{
-        console.log(res);
-        setData(res.data);
-        console.log(data);
-      }).catch((err)=>{
-        console.log(err);
-      })
-    }
-    infohandler()
+    // async function infohandler()
+    // {
+    //   // await axios.get("https://studentdb.pythonanywhere.com/students/",{}).then((res)=>{
+    //   //   console.log(res);
+    //   //   setData(res.data);
+    //   //   console.log(data);
+    //   // }).catch((err)=>{
+    //   //   console.log(err);
+    //   // })
+    //   fetch(
+    //     "https://studentdb.pythonanywhere.com/students/")
+    //                 .then((res) => res.json())
+    //                 .then((json) => {
+    //                     this.setState({
+    //                         items: json,
+    //                         DataisLoaded: true
+    //                     });
+    //                 })
+        
+    // }
+    // infohandler()
+    const url = "https://6aa5-112-196-153-10.ngrok.io/students/";
+
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
   },[])
  
 
