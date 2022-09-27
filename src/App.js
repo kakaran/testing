2741f2054f2d0fc860c7eb282ebcc49dec738b30
Karan Kapoor <kk8658141@gmail.com>
2022-09-27 12:58:39 +0530

    add the file s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect } from "react";
+import axios from 'axios';
 function App() {
+
+  useEffect(()=>{
+    
+    async function infohandler()
+    {
+      await axios.get("http://07b3-112-196-153-10.ngrok.io/students/").then((res)=>{
+        console.log(res);
+      }).catch((err)=>{
+        console.log(err);
+      })
+    }
+    infohandler()
+  },[])
+ 
+
+  // let LocalTiming = new Date().toLocaleTimeString();
+  // let LocalDateing = new Date().toLocaleDateString();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <h1 className="App">Hello My name is karan kapoor 2</h1>
+    </>
+  );}
 
 export default App;
